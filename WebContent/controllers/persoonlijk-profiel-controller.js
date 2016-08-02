c6215746119eac08d78f3477062f737cc4b3d2e6
rmvanderspek <rmvanderspek@gmail.com>
2016-08-02 17:18:16 +0200

    Add profile details and mock data

--- WebContent/controllers/persoonlijk-profiel-controller.js
+++ WebContent/controllers/persoonlijk-profiel-controller.js
@@ -1,5 +1,5 @@
 angular.module("Knowl").controller("PersoonlijkProfielCtrl",
-		[ "$scope", "$http", function($scope) {
+		[ "$scope", "$http", "$routeParams", function($scope, $http, $routeParams) {
 			$scope.getProfiles = function() {
 //				 As long as this doesn't work return a mock-array
 //				 $http({method : 'GET', url : "resources/personalprofiles"})
@@ -14,13 +14,23 @@ angular.module("Knowl").controller("PersoonlijkProfielCtrl",
 				// Mock array
 				return [ {
 					name : "Java Engineer",
+					description: "Een Java ontwikkelaar houd zich bezig met het ontwikkelen van de business logica  " +
+							"van applicaties (back-end).  Hij of zij is bovendien " +
+							"verantwoordelijk voor het onderhoud en het testen van de applicatie en het " +
+							"optimaliseren hiervan.",
 					id : 0
 				}, {
 					name : "Front-end developer",
+					description: "Een Front-end developer " +
+							"is verantwoordelijk is voor de technische kant van de visuele (voor)kant " +
+							"van een applicatie, website of programma. Hij of zij zorgt voor de verbinding tussen " +
+							"front-end (en het gebruik daarvan) en de back-end (waar de business logica plaatsvindt).",
 					id : 1
 				} ];
 				
 			};
 			$scope.profiles = $scope.getProfiles();
+			
+			$scope.profileDetails = $scope.profiles[$routeParams.id];
 
 		} ]);
