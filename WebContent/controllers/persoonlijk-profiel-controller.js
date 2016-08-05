2823fd24458910ba44a276aed75ce3e6e73d6042
Hanzeltog <me_sang@hotmail.com>
2016-08-05 11:02:33 +0200

    Now shows right competences with selected profile

--- WebContent/controllers/persoonlijk-profiel-controller.js
+++ WebContent/controllers/persoonlijk-profiel-controller.js
@@ -8,17 +8,17 @@ angular.module("Knowl").controller("PersoonlijkProfielCtrl", ["$scope", "$routeP
 		
 		$scope.select = function(index) {
 			$scope.selected = $scope.allProfiles[index].name;
-		}
+		};
 		
 		$scope.showAddCompetence = function() {
 			$scope.popAddKnowledgeProfile = false; // Make sure the other is closed
 			$scope.popAddCompetence = !$scope.popAddCompetence;
-		}
+		};
 		
 		$scope.showAddKnowledgeProfile = function() {
 			$scope.popAddCompetence = false; // Make sure the other is closed
 			$scope.popAddKnowledgeProfile = !$scope.popAddKnowledgeProfile;
-		}
+		};
 		
 		// Save a new competence --> needs to be implemented
 		$scope.save = function() {
